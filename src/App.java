06ae07d7c74d9366ce69b86437c572ddd37990ba
Shubhrose Singh <shubhroses@gmail.com>
2022-09-07 22:04:19 -0700

    Did another 30 min

--- src/App.java
+++ src/App.java
@@ -1,3 +1,4 @@
+import java.util.Arrays;
 import java.util.Date;
 
 import javax.sound.midi.Soundbank;
@@ -32,6 +33,69 @@ public class App {
         System.out.println(message.endsWith("World")); 
         System.out.println(message);
 
-        //48.12 in https://www.youtube.com/watch?v=eIrMbAQSU34&ab_channel=ProgrammingwithMosh
+        System.out.println(message.replace("Hello", "What A"));
+        System.out.println(message);
+
+        // Any methods that modify a string create a copy of the string
+        System.out.println(message.toLowerCase());
+
+
+        System.out.println("Hello \"World\"");
+        System.out.println("c: \\Windows\\..");
+        System.out.println("c: \nWindows\\..");
+        System.out.println("c: \tWindows\\..");
+
+        /*
+         * 
+         * 
+        int[] numbers = new int[5];
+        numbers[0] = 1;
+        numbers[1] = 2;
+         * 
+         */
+        
+        int[] numbers = {2, 3, 5, 1, 4};
+        System.out.println(numbers.length); 
+        Arrays.sort(numbers);
+        
+        System.out.println(Arrays.toString(numbers));
+
+        // Method overloading, implementing the same method but for different data types
+
+        //Multi dimensional array
+        int[][] nums = new int[2][3];
+        nums[0][0] = 1;
+        System.out.println(Arrays.deepToString(nums));
+
+        int[][] newNums = {{1, 2, 3}, {4, 5, 6}};
+        System.out.println(Arrays.deepToString(newNums));
+
+        final float PI = 3.14F; //Treated as a constant 
+        System.out.println(PI);
+
+        //Arithmetic Expressions
+        System.out.println(10/3);
+        System.out.println((double)10/(double)3);
+
+        int z = 1;
+        System.out.println(z++);
+
+        short x = 1;
+        int y = x+2;
+        // Short has 2 bytes, int has 4 bytes. 
+        // It create a new x of type int and then adds, this is implicit casting 
+        //byte > short > int > long > float > double
+        System.out.println(y);
+
+        double a = 1.1;
+        int b = (int)a + 2;
+        System.out.println(b);
+
+
+        double strToInt = Double.parseDouble("123.1");
+        System.out.println(strToInt);
+
+        int result = Math.round(1.1F)
+        //1:18.00 in https://www.youtube.com/watch?v=eIrMbAQSU34&ab_channel=ProgrammingwithMosh
     }
 }
